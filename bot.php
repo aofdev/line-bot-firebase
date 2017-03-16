d5c6426148f4fac3a3c28272a6511c6171ac0a73
Visarut Phusua <aof.dev@gmail.com>
2017-03-16 23:41:42 +0700

    update image bot.php

--- bot.php
+++ bot.php
@@ -97,6 +97,39 @@ if (!is_null($events['events'])) {
             curl_close($ch);
 
             echo $result . "\r\n";
+        }elseif ($event['type'] == 'message' && $event['message']['type'] == 'image'){
+            // Get image sent
+            $originalContentUrl = $event['message']['originalContentUrl'];
+            $previewImageUrl = $event['message']['previewImageUrl'];
+            // Get replyToken
+            $replyToken = $event['replyToken'];
+
+            // Build message to reply back
+            $messages = [
+                'type' => 'image',
+                'originalContentUrl' => $originalContentUrl,
+                'previewImageUrl' => $previewImageUrl,
+            ];
+
+            // Make a POST Request to Messaging API to reply to sender
+            $url = 'https://api.line.me/v2/bot/message/reply';
+            $data = [
+                'replyToken' => $replyToken,
+                'messages' => [$messages],
+            ];
+            $post = json_encode($data);
+            $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            $result = curl_exec($ch);
+            curl_close($ch);
+
+            echo $result . "\r\n";
         }
     }
 }
