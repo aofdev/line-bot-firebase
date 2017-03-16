fb8507c67b8f19458b862a6e0193e231fb560b5f
Visarut Phusua <aof.dev@gmail.com>
2017-03-16 23:01:51 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -14,6 +14,31 @@ if (!is_null($events['events'])) {
             $text = $event['message']['text'];
             // Get replyToken
             $replyToken = $event['replyToken'];
+            
+             if ($event['message']['text'] == 'hi' || $event['message']['text'] == 'hello'){
+                $messages = [
+                    'type' => 'sticker',
+                    'packageId' => 4,
+                    'stickerId' => 1,
+                ];
+                $url = 'https://api.line.me/v2/bot/message/reply';
+                $data = [
+                    'replyToken' => $replyToken,
+                    'messages' => [$messages],
+                ];
+                $post = json_encode($data);
+                $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+                $ch = curl_init($url);
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+                curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+                $result = curl_exec($ch);
+                curl_close($ch);
+
+                echo $result . "\r\n";
+            }
 
             // Build message to reply back
             $messages = [
