9b3a88ebb0727e23eb6fd724b72f0bfe1aec9754
Visarut Phusua <aof.dev@gmail.com>
2017-03-16 22:17:10 +0700

    update sticker bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = '';
+$access_token = 'D8p6zodWI7B3lFnlf+Ejqko3sbgRsYOfxYIszOJw2GzPR9EjCOcZVoon6ytNMd7JcC/O1YzhplEkLNr4Y/QpT0MWAC0f0YR6ID6hyRI1MfMKs/ouGeIB6S6aDs9FGbyWzABgfqhCBOTnXHkNAJU1/QdB04t89/1O/w1cDnyilFU=';
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
@@ -19,6 +19,39 @@ if (!is_null($events['events'])) {
             $messages = [
                 'type' => 'text',
                 'text' => $text
+            ];
+
+            // Make a POST Request to Messaging API to reply to sender
+            $url = 'https://api.line.me/v2/bot/message/reply';
+            $data = [
+                'replyToken' => $replyToken,
+                'messages' => [$messages],
+            ];
+            $post = json_encode($data);
+            $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+            $result = curl_exec($ch);
+            curl_close($ch);
+
+            echo $result . "\r\n";
+        }elseif ($event['type'] == 'message' && $event['message']['type'] == 'sticker'){
+            // Get sticker sent
+            $packageId = $event['message']['packageId'];
+            $stickerId = $event['message']['stickerId'];
+            // Get replyToken
+            $replyToken = $event['replyToken'];
+
+            // Build message to reply back
+            $messages = [
+                'type' => 'sticker',
+                'packageId' => $packageId,
+                'stickerId' => $stickerId,
             ];
 
             // Make a POST Request to Messaging API to reply to sender
